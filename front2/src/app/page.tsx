cbcbc14cb6ebc94e7fc09a160d6575c117edf98e
lucasramallo <lucasdebrito.jd@gmail.com>
2025-04-15 19:25:17 -0300

    tentando resolver submodule

--- front2/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import HomePage from './pages/Home'
-import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
-        
-export default function Home() {
-  return <HomePage />
-}
