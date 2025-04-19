46d35abc75630f6cdb182099827006327e208411
PML4 <pedrolucasmelo051@gmail.com>
2025-04-19 11:24:21 -0300

    removendo metodos desnecessarios no PostRepository

--- back/src/main/java/br/edu/ifpb/diario/diario/repository/PostRepository.java
+++ back/src/main/java/br/edu/ifpb/diario/diario/repository/PostRepository.java
@@ -1,8 +1,6 @@
 package br.edu.ifpb.diario.diario.repository;
 
 import br.edu.ifpb.diario.diario.domain.Post;
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,11 +9,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PostRepository extends JpaRepository<Post, UUID> {
 
-    List<Post> findAll();
-
-    Optional<Post> findById(UUID id);
-
-    Post save(Post post);
-
-    void deleteById(UUID id);
 }
