e470c27fb65ed13e4333bab421a5ada5747d870a
PML4 <pedrolucasmelo051@gmail.com>
2025-04-19 12:12:01 -0300

    Criando PostServiceImpl e implementando seus respectivos metodos.

--- back/src/main/java/br/edu/ifpb/diario/diario/service/PostService.java
+++ back/src/main/java/br/edu/ifpb/diario/diario/service/PostService.java
@@ -2,16 +2,17 @@ package br.edu.ifpb.diario.diario.service;
 
 import br.edu.ifpb.diario.diario.domain.Post;
 import java.util.List;
+import java.util.UUID;
 
-public interface PostService {
+public interface PostService  {
 
     List<Post> getAllPosts();
 
-    Post getPostById(Long id);
+    Post getPostById(UUID id);
 
     Post createPost(Post post);
 
-    Post updatePost(Long id, Post post);
+    Post updatePost(UUID id, Post post);
 
-    void deletePost(Long id);
+    void deletePost(UUID id);
 }
