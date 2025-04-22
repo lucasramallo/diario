2ab33e4a913391d63a9c92083733aa76f4ca7c31
Isaac Alves Leal de Oliveira <isaacalves2406@gmail.com>
2025-04-21 21:57:32 -0300

    Controller Finalizado

--- back/src/main/java/br/edu/ifpb/diario/diario/controller/PostController.java
+++ back/src/main/java/br/edu/ifpb/diario/diario/controller/PostController.java
@@ -1,7 +1,9 @@
 package br.edu.ifpb.diario.diario.controller;
 
 import br.edu.ifpb.diario.diario.config.FileStorageProperties;
+import br.edu.ifpb.diario.diario.domain.Post;
 import br.edu.ifpb.diario.diario.dtos.ImageDTO;
+import br.edu.ifpb.diario.diario.dtos.PostResquestDTO;
 import br.edu.ifpb.diario.diario.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +14,9 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -20,11 +24,36 @@ public class PostController {
     private final Path fileStorageLocation;
 
     @Autowired
-    private PostService fileService;
+    private PostService postService;
 
     public PostController(FileStorageProperties fileStorageProperties) {
         this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir())
                 .toAbsolutePath().normalize();
+    }
+
+    @PostMapping("/{posts}")
+    public ResponseEntity<Post> save(@RequestBody PostResquestDTO postRequestDTO) {
+        Post savedPost = postService.createPost(postRequestDTO, null, null);
+        if (savedPost == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(savedPost);
+    }
+
+    @GetMapping("/posts{id}")
+    public ResponseEntity<Post> getById(@PathVariable UUID id) {
+        Post post = postService.getPostById(id);
+        if (post != null) {
+            return ResponseEntity.ok(post);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/{posts}")
+    public ResponseEntity<List<Post>> getAll() {
+        List<Post> posts = postService.getAllPosts();
+        return ResponseEntity.ok(posts);
     }
 
     @PostMapping("/upload")
@@ -35,7 +64,7 @@ public class PostController {
             Path targetLocation = fileStorageLocation.resolve(fileName);
             file.transferTo(targetLocation);
 
-            ImageDTO imageDTO = fileService.uploadImage(targetLocation.toString(), file.getOriginalFilename());
+            ImageDTO imageDTO = postService.uploadImage(targetLocation.toString(), file.getOriginalFilename());
 
             return ResponseEntity.ok(imageDTO);
         } catch (IOException ex) {
